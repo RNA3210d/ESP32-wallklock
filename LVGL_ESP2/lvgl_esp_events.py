06c70cb1b28df12d8e9a4e717ccf40afebabf3d5
Naveen A.B. <53806497+RNA3210d@users.noreply.github.com>
2026-02-16 06:06:39 +0530

    Add files via upload

---
+++ LVGL_ESP2/lvgl_esp_events.py
@@ -0,0 +1,24 @@
+
+def backlightset(event_struct):
+    return
+
+
+def netconnect(event_struct):
+    return
+
+
+def startchrono(event_struct):
+    return
+
+
+def pschrono(event_struct):
+    return
+
+
+def rstchrono(event_struct):
+    return
+
+
+def cpynote(event_struct):
+    return
+
